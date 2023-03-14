d1ad75bec04c5b3f6d1c5161536a1b99a41c6ae9
SangWon7242 <gotkdqja@gmail.com>
2023-03-14 17:16:48 +0900

    Question, QuestionRepository 생성 및 테스트

--- src/main/java/com/sbs/exam/sbb/MainController.java
+++ src/main/java/com/sbs/exam/sbb/MainController.java
@@ -1,14 +1,19 @@
 package com.sbs.exam.sbb;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+  private final QuestionRepository questionRepository;
   @RequestMapping("/sbb")
   @ResponseBody
-  public String sbb() {
-    return "메인";
+  public List<Question> sbb() {
+    return questionRepository.findAll();
   }
 }
