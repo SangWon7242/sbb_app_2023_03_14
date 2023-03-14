ffbac8ac0dda9980c51a327643f4898a9efc38a2
SangWon7242 <gotkdqja@gmail.com>
2023-03-14 17:12:41 +0900

    세팅

---
+++ src/test/java/com/sbs/exam/sbb/Sbb20230314ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbs.exam.sbb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb20230314ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
