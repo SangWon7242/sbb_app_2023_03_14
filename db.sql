28d4fe2111f166a2aa73acf19c34d8db7269c3bd
SangWon7242 <gotkdqja@gmail.com>
2023-03-16 16:29:16 +0900

    Answer Entity, Answer Repository 생성, DB Answer 테이블 만듦

--- db.sql
+++ db.sql
@@ -23,3 +23,26 @@ INSERT INTO question
 SET create_date = NOW(),
 `subject` = '제목 3',
 content = '내용 3';
+
+CREATE TABLE answer (
+    id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    create_date DATETIME NOT NULL,
+    content TEXT,
+    question_id INT(10) UNSIGNED NOT NULL
+);
+
+# 답변 데이터 생성
+INSERT INTO answer
+SET create_date = NOW(),
+content = '답변 1',
+question_id = 1;
+
+INSERT INTO answer
+SET create_date = NOW(),
+content = '답변 2',
+question_id = 2;
+
+INSERT INTO answer
+SET create_date = NOW(),
+content = '답변 3',
+question_id = 3;
